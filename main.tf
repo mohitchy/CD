88317dd4be6bf84fb966c3f755285ee01679d799
mohitchy <61151798+mohitchy@users.noreply.github.com>
2020-09-25 19:06:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "techlanders-statefile"
+    bucket  = "mohitterraform"
     key  = "terraform/state"
-    region = "us-east-2"
+    region = "us-east-1"
 #   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
 #   secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
   }
@@ -12,13 +12,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "myawsserver" {
+resource "aws_instance" "mohitawsserver" {
   ami = "ami-0b16724fe8e66e4ec"
-  key_name = "gagan-cicd"
+  key_name = "mohit-cicd"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Gagan-Ubuntu-Server"
+    Name = "Mohit-Ubuntu-Server"
     Env = "Dev"
   }
   provisioner "local-exec" {
@@ -27,18 +27,18 @@ resource "aws_instance" "myawsserver" {
  
 provisioner "remote-exec" {
     inline = [
-     "touch /tmp/gagandeep"
+     "touch /tmp/mohit"
      ]
  connection {
     type     = "ssh"
     user     = "ubuntu"
     insecure = "true"
-    private_key = "${file("/tmp/gagan-cicd.pem")}"
-    host     =  aws_instance.myawsserver.public_ip
+    private_key = "${file("/tmp/mohit-cicd.pem")}"
+    host     =  aws_instance.mohitawsserver.public_ip
   }
 }
 }
 
 output "myawsserver-ip" {
-  value = "${aws_instance.myawsserver.public_ip}"
+  value = "${aws_instance.mohitawsserver.public_ip}"
 }
